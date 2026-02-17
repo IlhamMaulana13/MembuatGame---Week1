be2c9969c31d503fcd7dc9520fa9a46324448b33
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-02-17 21:20:00 +0700

    update bg color

---
+++ lib/game/fruit_catcher_game.dart
@@ -0,0 +1,8 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+class FruitCatcherGame extends FlameGame {
+
+  @override
+  Color backgroundColor() => const Color(0xFF87CEEB);
+}
