a8d0e093e955b3e5eb45350d19b7f144a951cf54
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-02-18 18:18:32 +0700

    Menambah class AudioManager

--- lib/game/managers/audio_manager.dart
+++ lib/game/managers/audio_manager.dart
@@ -1,10 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:flame_audio/flame_audio.dart';
+import 'package:flutter/foundation.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+class AudioManager {
+  
 }
