610eddc0ea2fcbd8f31b09e3d2e25afabf901ca6
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-02-18 18:20:52 +0700

    Update Geeters

--- lib/game/managers/audio_manager.dart
+++ lib/game/managers/audio_manager.dart
@@ -2,5 +2,20 @@ import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/foundation.dart';
 
 class AudioManager {
-  
+  // Singleton pattern
+ static final AudioManager _instance = AudioManager._internal();
+ factory AudioManager() => _instance;
+ AudioManager._internal();
+
+bool _isMusicEnabled = true;
+ bool _isSfxEnabled = true;
+ double _musicVolume = 0.7;
+ double _sfxVolume = 1.0;
+
+ // Getters
+ bool get isMusicEnabled => _isMusicEnabled;
+ bool get isSfxEnabled => _isSfxEnabled;
+ double get musicVolume => _musicVolume;
+ double get sfxVolume => _sfxVolume;
+
 }
