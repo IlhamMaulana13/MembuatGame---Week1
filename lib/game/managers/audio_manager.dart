8e519abed1ef36c110af091d44bd75a0948982a5
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-02-18 19:03:02 +0700

    disablelee music

--- lib/game/managers/audio_manager.dart
+++ lib/game/managers/audio_manager.dart
@@ -135,4 +135,12 @@ class AudioManager {
       resumeBackgroundMusic();
     }
   }
+
+  /// Disable music
+ void disableMusic() {
+ if (_isMusicEnabled) {
+ _isMusicEnabled = false;
+ pauseBackgroundMusic();
+ }
+ }
 }
