2f9997e6424d4b25ebaec12e80583a8b350e3e15
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-02-18 19:14:00 +0700

    update file baru basket.dart

---
+++ lib/game/components/basket.dart
@@ -0,0 +1,14 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+class Basket extends PositionComponent with HasGameRef, CollisionCallbacks {
+  Basket() : super(size: Vector2(80, 60));
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    position = Vector2(gameRef.size.x / 2, gameRef.size.y - 100);
+    anchor = Anchor.center;
+    add(RectangleHitbox());
+  }
+}
